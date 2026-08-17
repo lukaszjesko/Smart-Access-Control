d1ce15aadff152a4029a621361ec67a22f43a5a6
LukasJ <lukasz.jeskoo@gmail.com>
2026-08-17 21:11:23 +0200

    wyrzucenie wyświetlacza, rozwiązanie problemu pada, dodanie enum i kontroli wpisywania pinu

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,98 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Keypad.h>
+#include <LiquidCrystal_I2C.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const byte ROWS = 4; 
+const byte COLS = 4;
+
+byte rowPins[ROWS] = {6,7,8,9};
+byte colPins[COLS] = {5,4,3,2};
+
+char keys[ROWS][COLS] = {
+ {'1','2','3','A'},
+ {'4','5','6','B'},
+ {'7','8','9','C'},
+ {'*','0','#','D'}
+ };
+
+ enum StanSystemu {
+    CZEKAM_NA_PIN,
+    WPISYWANIE_PINU,
+    ZAAKCEPTOWANY,
+    ODRZUCONY
+ };
+
+ StanSystemu obecnyStan =  CZEKAM_NA_PIN;
+ String tajnyPin = "1234";
+ String wpisanyPin = "";
+ 
+ Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("System kontroli dostepu gotowy.");
+  Serial.println("Wprowadz PIN");
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  char key = keypad.getKey();
+  switch (obecnyStan){
+    case CZEKAM_NA_PIN:
+      if (key){
+        wpisanyPin+= key;
+        Serial.print("*");
+        obecnyStan = WPISYWANIE_PINU;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+      }
+    break;
+
+    case WPISYWANIE_PINU:
+      if(key){
+        wpisanyPin += key;
+        Serial.print("*");
+
+        if (wpisanyPin.length() == 4){
+          Serial.println();
+
+          if (wpisanyPin == tajnyPin){
+            obecnyStan = ZAAKCEPTOWANY;
+          }else{
+            obecnyStan = ODRZUCONY;
+          }
+        
+        }
+      }
+    break;
+
+    case ZAAKCEPTOWANY:
+      Serial.println("-->DOSTEP PRZYZNANY");
+      Serial.println("-->ZAMEK OTWARTY NA 3 SEKUNDY");
+      delay(3000);
+      Serial.println("-->ZAMEK ZAMKNIETY");
+      Serial.println("\n-->WPROWADZ PIN:");
+
+      wpisanyPin = "";
+      obecnyStan = CZEKAM_NA_PIN;
+
+
+    break;
+
+    case ODRZUCONY:
+    Serial.println("-->ODMOWA DOSTEPU");
+    delay(3000);
+    Serial.println("\n-->SPROBOJ PONOWNIE, WPROWADZ PIN:");
+    wpisanyPin = "";
+    obecnyStan = CZEKAM_NA_PIN;
+
+    
+    break;
+  }
+      
+      
+
+    
+  
 }
